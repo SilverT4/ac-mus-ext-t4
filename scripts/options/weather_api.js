d3a32c99c7239193759761a3ea96b45d3677a609
Silver <devinpschweiss@gmail.com>
2024-05-27 17:44:17 -0400

    update_location thing

--- scripts/options/weather_api.js
+++ scripts/options/weather_api.js
@@ -1,6 +1,7 @@
 'use strict';
 let apiKey;
 let loc;
+let lastSearch;
 
 function get_settings() {
     chrome.storage.sync.get({
@@ -28,7 +29,10 @@ function get_settings() {
         document.getElementById("iata-code").value = items.WxIataCode;
         var ls = items.WxLastSearch ? items.WxLastSearch : "N/A";
         document.querySelector('label[for="last-used-search"]').innerHTML = "<span><span></span></span>Last used search location: " + ls;
-        if (ls != "N/A") document.getElementById("last-used-search").disabled = false;
+        if (ls != "N/A") {
+            document.getElementById("last-used-search").disabled = false;
+            lastSearch = ls;
+        }
     });
 }
 
@@ -55,7 +59,7 @@ function save_settings() {
     let WxMetarCode = document.getElementById("metar-code").value;
     let WxIataCode = document.getElementById("iata-code").value;
     var piss = document.getElementById("search-query").value; // search query box value
-    let WxLastSearch = piss ? piss : "";
+    let WxLastSearch = piss ? piss : "N/A";
 
     chrome.storage.sync.set({
         WxApiKey,
@@ -72,7 +76,48 @@ function save_settings() {
     });
 }
 function update_location() {
-    // gotta add onclicks first
+    // gotta add onclicks
+    let LocationType = "";
+    let pp = document.querySelectorAll('input[name="location-type"]');
+
+    pp.forEach(element => {
+        var id = element.id;
+        if (element.checked) LocationType = id;
+    });
+
+    var locationTypes = ["local-ip", "ip-input", "lat-long", "zip-code", "uk-postal", "canada-post", "metar", "iata", "search", "last-used-search"];
+    
+    switch (LocationType) {
+        case 'local-ip':
+            loc = 'auto:ip';
+            return;
+        case 'ip-input':
+            loc = document.getElementById("ip-address").value;
+            return;
+        case 'lat-long':
+            let [lat, long] = [document.getElementById('latitude').value, document.getElementById('longitude').value];
+            loc = `${lat},${long}`;
+            return;
+        case 'zip-code':
+            loc = document.getElementById("us-zip").value;
+            return;
+        case 'uk-postal':
+            loc = document.getElementById("uk-post").value;
+            return;
+        case 'canada-post':
+            loc = document.getElementById('ca-post').value;
+        case 'metar':
+            loc = `metar:${document.getElementById('metar-code').value}`;
+        case 'iata':
+            loc = `iata:${document.getElementById('iata-code').value}`;
+        case 'search':
+            // nothing until i figure out search lmao
+        case 'last-used-search':
+            loc = lastSearch;
+        default:
+            loc = 'auto:ip';
+    }
+    loc = 'auto:ip'; // In case 
 }
 function get_weather() {
     apiKey = document.getElementById("wxapi-key").value;
